5c9d0b92669a2f84513e60b70122cd720b5979b0
Augusto Corvalan <augustocorvalan@gmail.com>
2014-04-10 18:51:11 -0400

    wired up server module with tempEmitter and socket

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var http = require("http");
 var url = require("url");
+var server = http.createServer();
+var io = require("socket.io").listen(server);
+var tempEmitter = require("./tempEmitter");
+var moment = require("moment");
+
+server.listen(8000);
 
 function start(route, handle) {
-
     function onRequest(req, res) {
 		var pathname = url.parse(req.url).pathname;
-
 		console.log("request for " + pathname + " received");	
 		route(handle, pathname, res);
 	}
 
-	http.createServer(onRequest).listen(8080);	
+	server.on("request", onRequest);
+
+	io.sockets.on('connection', function (socket) {
+		tempEmitter.on("temp:loaded", function (temp) {
+			var timeStamp = moment().format("ddd, h:mm:ss a");
+			socket.emit('tempReading', { temp: temp, timeStamp: timeStamp});
+		});
+	});
 }
 
 exports.start = start;
